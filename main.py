76f4857a51b955bd3d646b1b9385cc18a428bda6
Abotishere <aranyarayed06@gmail.com>
2025-10-28 19:01:39 +0530

    Work on the project start

---
+++ main.py
@@ -0,0 +1,6 @@
+from tkinter import *
+# import pygame as pg (pygame download hochena)
+import os
+import requests
+
+from playback import audio_Control
---
+++ playback.py
@@ -0,0 +1,47 @@
+class audio_Control:
+    def __init__(self):
+        pass
+
+    def play_Audio(self):
+        pass
+
+    def pause_Audio(self):
+        pass
+
+    def prev_Audio(self):
+        pass
+
+    def next_Audio(self):
+        pass
+
+    def repeat_Audio(self):
+        pass
+
+    def shuffle_Playlist(self):
+        pass
+
+    def seek_Audio(self):
+        pass
+
+    def change_Volume(self, Inc = False, Dec = False):
+        if Inc:
+            #increase volume
+            return
+        elif Dec:
+            #decrease volume
+            return
+        else:
+            #do nothing
+            return
+        
+    def add_to_Favourites(self):
+        pass
+
+    def view_Playlist(self):
+        pass
+
+    def view_Lyrics(self):
+        pass
+
+    def organize_Folder(self):
+        pass
